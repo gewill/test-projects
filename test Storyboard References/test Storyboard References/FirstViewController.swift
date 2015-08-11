5e29f9a804bcfb27d848ea8b4e136db0af1c8ba4
gewill <531sunlight@gmail.com>
2015-08-11 18:46:46 +0800

    add test Storyboard References

---
+++ test Storyboard References/test Storyboard References/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  test Storyboard References
+//
+//  Created by Will Ge on 8/11/15.
+//  Copyright © 2015 gewill.org. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
