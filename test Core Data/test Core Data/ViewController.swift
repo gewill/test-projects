41bf215d94fcc623296f22fed5af23ea2aafbcc0
gewill <531sunlight@gmail.com>
2015-08-21 22:26:43 +0800

    add test Core Data

---
+++ test Core Data/test Core Data/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  test Core Data
+//
+//  Created by Will Ge on 8/21/15.
+//  Copyright (c) 2015 gewill.org. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBOutlet weak var textUserName: UITextField!
+    @IBOutlet weak var textPassword: UITextField!
+    
+    @IBAction func saveButton(sender: AnyObject) {
+        
+        // Cora Data already settled done in AppDelegate.swift
+        // get the ManagedObjectContext
+        var appDel = UIApplication .sharedApplication().delegate as! AppDelegate
+        var context: NSManagedObjectContext = appDel.managedObjectContext!
+        
+        // get the ManagedObject
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context) as! NSManagedObject
+        
+        // set value for key
+        newUser.setValue("" + textUserName.text, forKey: "username")
+        newUser.setValue("" + textPassword.text, forKey: "password")
+        
+        // save
+        context.save(nil)
+        print("New username: \(textPassword.text) and password: \(textPassword.text) saved successfully.\n")
+        
+        
+    }
+  
+    @IBAction func loadButton(sender: AnyObject) {
+        
+        var appDel = UIApplication .sharedApplication().delegate as! AppDelegate
+        var context: NSManagedObjectContext = appDel.managedObjectContext!
+        
+        // add FetchRequest and add Predicate/ SortDescriptor etc.
+        var request = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        request.predicate = NSPredicate(format: "username = %@", "" + textUserName.text)
+        
+        var requests: NSArray = context.executeFetchRequest(request, error: nil)!
+        
+        if (requests.count > 0) {
+            
+            var res = requests[0] as! NSManagedObject
+            textUserName.text = res.valueForKeyPath("username") as! String
+            textPassword.text = res.valueForKeyPath("password") as! String
+            
+        } else {
+            
+            print("0 Results Returned... Poterial Error.\n")
+        }
+        
+    }
+}
+
